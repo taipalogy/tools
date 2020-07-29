bd8388c3208e84290b81a9a430fc229ecf0e36e2
Wessen Sotron <sjhuaii@gmail.com>
2020-07-29 20:15:12 +0800

    second commit

---
+++ dropletters.ts
@@ -0,0 +1,65 @@
+import * as fs from 'fs';
+import * as readline from 'readline'; // TODO: install node
+import { Client } from '../taipa/src/client';
+import { graphAnalyzeTonal } from '../taipa/src/tonal/analyzer';
+import {
+  TonalLetterTags,
+  lowerLettersTonal,
+} from '../taipa/src/tonal/version2';
+import { AlphabeticGrapheme } from '../taipa/src/unit';
+
+/**
+ * drop one letter of stop finals for the third and fifth checked tones.
+ */
+
+let path = '../markdowns/output.md';
+
+const readInterface = readline.createInterface(
+  fs.createReadStream('../markdowns/example.md')
+);
+
+fs.appendFileSync(path, '', {
+  flag: 'w',
+});
+
+const buffer: string[] = [];
+const cli = new Client();
+
+readInterface.on('line', (l: string) => {
+  let aLine = '';
+  // tokenizer
+  const tokens = l.match(/\w+/g);
+  if (tokens) {
+    for (const tok of tokens) {
+      const gs = graphAnalyzeTonal(tok);
+      // console.log(gs)
+      const thirdsFifths = gs.filter(
+        it => it.letter.literal === TonalLetterTags.hh
+      );
+      if (thirdsFifths.length > 0) {
+        const gsAfter = gs.map(it => {
+          if (it.letter.literal === TonalLetterTags.hh) {
+            return new AlphabeticGrapheme(
+              lowerLettersTonal.get(TonalLetterTags.h)
+            );
+          } else {
+            return it;
+          }
+        });
+        // console.log(gsAfter);
+        aLine += gsAfter.map(it => it.letter.literal).join('');
+      } else {
+        aLine += tok;
+      }
+    }
+  }
+  buffer.push(aLine);
+});
+
+readInterface.on('close', () => {
+  for (let i in buffer) {
+    fs.appendFileSync(path, buffer[i] + '\n', {
+      flag: 'a+',
+    });
+  }
+});
