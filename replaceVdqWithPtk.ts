d816124794c184b6fd62a7a3379204be2b81bc87
Wessen Sotron <sjhuaii@gmail.com>
2020-11-05 15:39:01 +0800

    make it to work with taipa v0.7.2

--- replaceVdqWithPtk.ts
+++ replaceVdqWithPtk.ts
@@ -9,6 +9,7 @@ import {
 } from '../taipa/src/tonal/version2';
 import { AlphabeticLetter } from '../taipa/src/unit';
 import { TonalSyllable } from '../taipa/src/tonal/morpheme';
+import { graphAnalyzeTonal } from '../taipa/src/tonal/analyzer';
 
 /**
  * Replace letters v, d, and q with letters p, t, k. Replace letters p, t, and k with letters, ph, th, kh.
@@ -30,10 +31,7 @@ const cli = new Client();
 const vdqToPtk = new Map<string, string>()
   .set(TonalLetterTags.v, TonalLetterTags.p)
   .set(TonalLetterTags.d, TonalLetterTags.t)
-  .set(TonalLetterTags.q, TonalLetterTags.k)
-  .set(TonalLetterTags.p, TonalLetterTags.p + TonalLetterTags.h) // to be changed to TonalLetterTags.ph
-  .set(TonalLetterTags.t, TonalLetterTags.t + TonalLetterTags.h) // to be changed to TonalLetterTags.th
-  .set(TonalLetterTags.k, TonalLetterTags.k + TonalLetterTags.h); // to be changed to TonalLetterTags.kh
+  .set(TonalLetterTags.q, TonalLetterTags.k);
 
 readInterface.on('line', (l: string) => {
   let aLine = '';
@@ -41,6 +39,8 @@ readInterface.on('line', (l: string) => {
   const tokens = l.match(/\w+/g);
   if (tokens) {
     for (let i = 0; i < tokens.length; i++) {
+      // const gs = graphAnalyzeTonal(tokens[i]);
+      // console.log(tokens[i], gs.map(it => it.letter.literal).join(''));
       const seqs = cli.processTonal(tokens[i].toLowerCase()).letterSequences;
       // console.log(seqs);
       let isCapital: boolean[] = [];
@@ -67,39 +67,36 @@ readInterface.on('line', (l: string) => {
               it.toString() === TonalLetterTags.k)
         );
 
+        len = seqs
+          .map((val, k, arr) => (k < j ? val : []))
+          .map(it => it.length)
+          .reduce((prev, curr) => prev + curr);
+        if (s.letters[0].literal.toUpperCase() === tokens[i].charAt(len)) {
+          // if this letter is capital
+          isCapital[j] = true;
+        }
+
         if (initsVdq.length == 1) {
           const got = vdqToPtk.get(initsVdq[0].toString());
           if (got) {
-            len = seqs
-              .map((val, k, arr) => (k < j ? val : []))
-              .map(it => it.length)
-              .reduce((prev, curr) => prev + curr);
-            if (s.letters[0].literal.toUpperCase() === tokens[i].charAt(len)) {
-              // if this letter is capital
-              isCapital[j] = true;
-            }
-
             // to remove v, d, q from lowerLettersTonal
             s.replaceLetter(0, lowerLettersTonal.get(got));
             // console.log(`replacing with ${got} and get ${s.literal}`);
             syls.push(s);
           }
         } else if (initsPtk.length == 1) {
-          const got = vdqToPtk.get(initsPtk[0].toString());
-          if (got) {
-            // to add ph, th, kh to lowerLettersTonal
-            // the below line doesn't work before adding the above letters
-            s.replaceLetter(0, lowerLettersTonal.get(got));
-            syls.push(s);
-          }
+          // insert an h after p, t, k
+          s.insertLetter(1, lowerLettersTonal.get(TonalLetterTags.h));
+          // console.log(`replacing with ${initsPtk[0] + 'h'} and get ${s.literal}`);
+          syls.push(s);
         } else {
           syls.push(s);
         }
       }
       const word = seqs.map(x => x.map(y => y.toString()).join('')).join('');
       const wordAfter = syls.map(it => it.literal).join('');
-      // console.log(word, wordAfter);
-      if (word !== wordAfter) {
+      // console.log(tokens[i], word, wordAfter);
+      if (word !== wordAfter && tokens[i].length <= wordAfter.length) {
         let idx = 0;
         const len = tokens[i].length;
         let head = '';
